cd58e5cd1a9f381caee7ca99e20ce68f56505aba
Nitull <112264960+Nitull@users.noreply.github.com>
2022-08-30 16:43:19 +0530

    Add files via upload

---
+++ Home/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME PAGE</title>
+</head>
+<link rel="stylesheet" href="style.css">
+
+<body>
+<div>  
+        
+    <header class="header">
+    <div class="left">
+    <img src="the.png">
+    <div>Anime.to</div>
+
+</div>
+  <div-class="mid">
+    <div class="mid">
+        <ul class="navbar">
+            <li><a href="#" class=>Home</a></li>
+            <li><a href="#1">Movies</a></li>
+            <li><a href="#4">Tv series</a></li>
+            <li><a href="#2">Most popular</a></li>
+            <li><a href="#3">Top arriving</a></li>
+        </ul>
+    </div>
+  </div>
+  <div class="right">
+
+       <button class="btn">Instagram</button>
+       <button class="btn">Facebook</button>
+  </div>
+    </header>
+    <div class="container">
+        <h2>The best site to watch anime online for Free</h2>
+        <h7>Top search:One PieceNaruto: ShippudenJujutsu Kaisen 0 MovieNarutoBoruto: Naruto Next GenerationsDragon Ball SuperJujutsu Kaisen (TV)Black CloverHarem in the Labyrinth of Another World (Uncensored)Dragon Ball
+        </h7>
+    </div>
+        
+        <div class="bot"> 
+            <button class="bot">JOIN NOW</button>
+      </div>
+
+</body>
+</html>
